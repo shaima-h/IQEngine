c7674f95cdf7eb2ab3d7d2b632cfc55df1d02dec
shaima-h <shaima.hussaini@gmail.com>
2024-03-12 15:33:22 -0400

    add airview plugin folder

---
+++ plugins/src/airview/airview.py
@@ -0,0 +1,11 @@
+'''
+Steps to write a plugin
+- Create a folder in plugins/src (for example, myplugin)
+- Create the python file. (For example, myplugin/myplugin.py)
+- The folder and file name must match exactly.
+- look at the template plugin or another similar plugin to the one you want to make, as a starting point
+- sample_rate and center_freq are standard and must be included in output
+- There must be at least one custom param or the Run Plugin button will not show up.
+- data_output is a required part of the interface
+- data_type is a required part of the interface
+'''
